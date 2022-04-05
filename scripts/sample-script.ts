5b56fb2aaa96c1555bb39ad0b463be3e5d2a576a
Przemysław Trepka <ptrepka@Przemysaws-MacBook-Pro.local>
2022-04-05 11:54:50 +0200

    #1 add simple script

---
+++ scripts/sample-script.ts
@@ -0,0 +1,17 @@
+import {ethers} from "hardhat";
+
+const changeGreeting = async (greeting : string) => {
+  const greeter = await ethers.getContract("Greeter");
+
+  const tx = await greeter.setGreeting(greeting);
+  await tx.wait(1);
+
+  console.log("Greeting has been changed.");
+}
+
+changeGreeting("New greeting")
+.then(() => process.exit(0))
+.catch(e => {
+  console.log(e);
+  process.exit(1)
+})
