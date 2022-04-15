6f9ba56a7aab550329d7771233805949e325e2d2
Piotr Obuchowski <pobuchowski@valuelogic.one>
2022-04-15 13:23:28 +0200

    4: added basic tests

--- contracts/users/Users.sol
+++ contracts/users/Users.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-import "./model/Model.sol";
+import "./../model/Model.sol";
 
 contract Users {
 
---
+++ test/users/Users.test.js
@@ -0,0 +1,46 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+const { BigNumber } = require("ethers");
+
+describe("Users contract", function () {
+  it("deploys users contract without error", async function () {
+    const Users = await ethers.getContractFactory("Users");
+
+    const usersContract = await Users.deploy();
+
+    expect(usersContract).not.to.be.null
+  });
+
+  it("deploys users contract with empty users list", async function () {
+    const Users = await ethers.getContractFactory("Users");
+
+    const usersContract = await Users.deploy();
+
+    const result = await usersContract.getUsers();
+
+    expect(result).to.eql([]);
+  });
+
+  it("should add user to users list", async function () {
+    const Users = await ethers.getContractFactory("Users");
+
+    const usersContract = await Users.deploy();
+
+    const walletAddress = await ethers.Wallet.createRandom().getAddress();
+
+    const nick = "EXPECTED_NICK";
+
+    await usersContract.addUser(false, walletAddress, nick);
+
+    const usersList = await usersContract.getUsers();
+
+    expect(usersList).to.have.lengthOf(1);
+
+    const user = usersList[0];
+
+    expect(user.wallet).to.equal(walletAddress)
+    expect(user.isAdmin).to.be.false
+    expect(user.nick).to.equal(nick)
+    expect(user.points).to.eql(BigNumber.from(0))
+  });
+});
