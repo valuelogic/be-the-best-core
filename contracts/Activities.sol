db6a82b121342afc932231d25cb5ec9054232a40
Maciej Czekaj <mczekaj@valuelogic.one>
2022-05-16 10:06:06 +0200

    #3 Added tests to Activities contract

--- contracts/Activities.sol
+++ contracts/Activities.sol
@@ -1,33 +1,45 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.8;
 
 import "./Activity.sol";
 
 contract Activities {
     Activity[] public activities;
 
+    event ActivityAdded(uint256 index, Activity activity);
+    event ActivityDeleted(uint256 index);
+    event ActivityUpdated(uint256 index, Activity oldActivity, Activity newActivity);
+
     constructor() {
         activities = new Activity[](0);
     }
 
-    function addActivity(Activity activity) public {
+    function addActivity(Activity activity) external {
         activities.push(activity);
+        emit ActivityAdded(activities.length - 1, activity);
     }
 
-    function getActivity(uint index) public view returns (Activity) {
+    function getActivity(uint index) external indexInBounds(index) view returns (Activity) {
         return activities[index];
     }
 
-    function getActivities() public view returns (Activity[] memory) {
+    function getActivities() external view returns (Activity[] memory) {
         return activities;
     }
 
-    function updateActivity(uint index, Activity activity) public {
+    function updateActivity(uint index, Activity activity) external indexInBounds(index) {
+        emit ActivityUpdated(index, activities[index], activity);
         activities[index] = activity;
     }
 
-    function deleteActivity(uint index) public {
+    function deleteActivity(uint index) external indexInBounds(index) {
         activities[index] = activities[activities.length - 1];
         activities.pop();
+        emit ActivityDeleted(index);
+    }
+
+    modifier indexInBounds(uint index) {
+        require(index < activities.length, 'Index out of bounds');
+        _;
     }
 }
--- test/Activities.test.ts
+++ test/Activities.test.ts
@@ -1,18 +1,14 @@
-import {deployContract, deployMockContract, MockProvider} from "ethereum-waffle";
-import {Wallet} from "ethers";
-import {expect} from "chai";
+import {deployContract, deployMockContract, MockProvider, solidity} from "ethereum-waffle";
+import {expect, use} from "chai";
 import ContractJson from "../artifacts/contracts/Activities.sol/Activities.json";
-import {Activities, Activities__factory, Activity, Activity__factory} from "../typechain-types";
+import {Activities, Activities__factory, Activity__factory} from "../typechain-types";
 import {ethers} from "hardhat";
-import exp from "constants";
+
+use(solidity);
 
 describe('Activities contract', () => {
     describe('deploy', () => {
-        let wallet: Wallet;
-
-        beforeEach(() => {
-            [wallet] = new MockProvider().getWallets();
-        })
+        let [wallet] = new MockProvider().getWallets();
 
         it('should deploy the contract', async () => {
             const contract = await deployContract(wallet, ContractJson, [], {gasLimit: 1000000});
@@ -20,22 +16,144 @@ describe('Activities contract', () => {
         });
     });
 
-    describe('addActivity', async () => {
+    describe('add activity', async () => {
         let contract: Activities;
-        let wallet: Wallet;
+        let [wallet] = new MockProvider().getWallets();
 
         beforeEach(async () => {
-            [wallet] = new MockProvider().getWallets();
             const factory = await ethers.getContractFactory<Activities__factory>('Activities', wallet);
             contract = await factory.deploy();
         });
 
-        it('should add an activity', async () => {
+        it('should emit ActivityAdded event', async () => {
             const activity = await deployMockContract(wallet, Activity__factory.abi);
 
-            await contract.addActivity(activity.address);
+            await expect(contract.addActivity(activity.address, {gasLimit: 1000000}))
+                .to.emit(contract, 'ActivityAdded').withArgs(0, activity.address);
+        });
+    });
+
+    describe('get activities', () => {
+        let contract: Activities;
+        let [wallet] = new MockProvider().getWallets();
+
+        beforeEach(async () => {
+            const factory = await ethers.getContractFactory<Activities__factory>('Activities', wallet);
+            contract = await factory.deploy();
+        });
+
+        it('should return an empty array', async () => {
+            const activities = await contract.getActivities();
+            expect(activities.length).to.equal(0);
+        });
+
+        it('should return an array with one activity', async () => {
+            const activity1 = await deployMockContract(wallet, Activity__factory.abi);
+            const activity2 = await deployMockContract(wallet, Activity__factory.abi);
+
+            await contract.addActivity(activity1.address, {gasLimit: 1000000});
+            await contract.addActivity(activity2.address, {gasLimit: 1000000});
+
+            const activities = await contract.getActivities();
+            expect(activities.length).to.equal(2);
+        });
+    });
+
+    describe('get activity', () => {
+        let contract: Activities;
+        let [wallet] = new MockProvider().getWallets();
+
+        beforeEach(async () => {
+            const factory = await ethers.getContractFactory<Activities__factory>('Activities', wallet);
+            contract = await factory.deploy();
+        })
+
+        it('should revert transaction when does not exist', async () => {
+            await expect(contract.getActivity(0)).to.be.revertedWith('Index out of bounds');
+        })
+
+        it('should return the activity', async () => {
+            const activity1 = await deployMockContract(wallet, Activity__factory.abi);
+            await contract.addActivity(activity1.address, {gasLimit: 1000000});
+
+            const activity2 = await deployMockContract(wallet, Activity__factory.abi);
+            await contract.addActivity(activity2.address, {gasLimit: 1000000});
+
+            const activityAddress = await contract.getActivity(1);
+            expect(activityAddress).to.equal(activity2.address);
+        })
+    });
+
+    describe('delete activity', async () => {
+        const [wallet] = new MockProvider().getWallets();
+        let contract: Activities;
+
+        beforeEach(async () => {
+            const factory = await ethers.getContractFactory<Activities__factory>('Activities', wallet);
+            contract = await factory.deploy();
+        })
+
+        it('should revert transaction when does not exist', async () => {
+            await expect(contract.deleteActivity(0, {gasLimit: 1000000})).to.be.revertedWith('Index out of bounds');
+        });
+
+        it('should delete activity', async () => {
+            const activity1 = await deployMockContract(wallet, Activity__factory.abi);
+            await contract.addActivity(activity1.address, {gasLimit: 1000000});
+
+            const activity2 = await deployMockContract(wallet, Activity__factory.abi);
+            await contract.addActivity(activity2.address, {gasLimit: 1000000});
+
+            await contract.deleteActivity(1, {gasLimit: 1000000});
             const activities = await contract.getActivities();
             expect(activities.length).to.equal(1);
+            expect(activities[0]).to.equal(activity1.address);
         });
+
+        it('should emit event', async () => {
+            const activity1 = await deployMockContract(wallet, Activity__factory.abi);
+            await contract.addActivity(activity1.address, {gasLimit: 1000000});
+
+            const activity2 = await deployMockContract(wallet, Activity__factory.abi);
+            await contract.addActivity(activity2.address, {gasLimit: 1000000});
+
+            await expect(contract.deleteActivity(1, {gasLimit: 1000000}))
+                .to.emit(contract, 'ActivityDeleted').withArgs(1);
+        });
+    });
+
+    describe('update activity', () => {
+        const [wallet] = new MockProvider().getWallets();
+        let contract: Activities;
+
+        beforeEach(async () => {
+            const factory = await ethers.getContractFactory<Activities__factory>('Activities', wallet);
+            contract = await factory.deploy();
+        })
+
+        it('should revert transaction when does not exist', async () => {
+            await expect(contract.updateActivity(0, '0x0000000000000000000000000000000000000000', {gasLimit: 1000000}))
+                .to.be.revertedWith('Index out of bounds');
+        });
+
+        it('should update the activity', async() => {
+            const activity1 = await deployMockContract(wallet, Activity__factory.abi);
+            await contract.addActivity(activity1.address, {gasLimit: 1000000});
+
+            const activity2 = await deployMockContract(wallet, Activity__factory.abi);
+            await contract.updateActivity(0, activity2.address, {gasLimit: 1000000});
+
+            const updatedActivity = await contract.getActivity(0, {gasLimit: 1000000});
+            await expect(updatedActivity).to.equal(activity2.address);
+        });
+
+        it('should emit event', async() => {
+            const activity1 = await deployMockContract(wallet, Activity__factory.abi);
+            await contract.addActivity(activity1.address, {gasLimit: 1000000});
+
+            const activity2 = await deployMockContract(wallet, Activity__factory.abi);
+            await expect(contract.updateActivity(0, activity2.address, {gasLimit: 1000000}))
+                .to.emit(contract, 'ActivityUpdated').withArgs(0, activity1.address, activity2.address);
+        })
     });
 });
