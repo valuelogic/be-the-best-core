7cdcd88d1497c3a44e2dbeb3d6af96a91fdbddae
Maciej Czekaj <mczekaj@valuelogic.one>
2022-05-23 09:15:35 +0200

    #3 Added s_ prefix to storage variables

--- contracts/Activities.sol
+++ contracts/Activities.sol
@@ -2,38 +2,38 @@
 pragma solidity ^0.8.8;
 
 contract Activities {
-    address[] public activities;
+    address[] public s_activities;
 
     event ActivityAdded(uint256 index, address activity);
     event ActivityDeleted(uint256 index);
     event ActivityUpdated(uint256 index, address oldActivity, address newActivity);
 
     function addActivity(address activity) external {
-        activities.push(activity);
-        emit ActivityAdded(activities.length - 1, activity);
+        s_activities.push(activity);
+        emit ActivityAdded(s_activities.length - 1, activity);
     }
 
     function getActivity(uint index) external indexInBounds(index) view returns (address) {
-        return activities[index];
+        return s_activities[index];
     }
 
     function getActivities() external view returns (address[] memory) {
-        return activities;
+        return s_activities;
     }
 
     function updateActivity(uint index, address activity) external indexInBounds(index) {
-        emit ActivityUpdated(index, activities[index], activity);
-        activities[index] = activity;
+        emit ActivityUpdated(index, s_activities[index], activity);
+        s_activities[index] = activity;
     }
 
     function deleteActivity(uint index) external indexInBounds(index) {
-        activities[index] = activities[activities.length - 1];
-        activities.pop();
+        s_activities[index] = s_activities[s_activities.length - 1];
+        s_activities.pop();
         emit ActivityDeleted(index);
     }
 
     modifier indexInBounds(uint index) {
-        require(index < activities.length, 'Index out of bounds');
+        require(index < s_activities.length, 'Index out of bounds');
         _;
     }
 }
