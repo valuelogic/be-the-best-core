65643a9aaf60eaaf47d42ebbff92739aaf85a7cf
Maciej Czekaj <mczekaj@valuelogic.one>
2022-07-13 10:35:22 +0200

    fixed unit tests

--- test/RequestsManager.test.ts
+++ test/RequestsManager.test.ts
@@ -1,13 +1,15 @@
-import {deployments, ethers} from "hardhat";
+import {deployments, ethers, waffle} from "hardhat";
 import {expect} from "chai";
-import {deployMockContract, MockContract} from "ethereum-waffle";
 import {
     Activity__factory,
-    Authorization__factory, Players__factory,
+    Authorization__factory,
+    Players__factory,
     Requests__factory,
-    RequestsManager, RequestsManager__factory
+    RequestsManager,
+    RequestsManager__factory
 } from "../typechain-types";
-import {SignerWithAddress} from "@nomiclabs/hardhat-ethers/signers";
+import {MockContract} from "ethereum-waffle";
+import {Wallet} from "ethers";
 
 describe('RequestsManager Contract', () => {
     describe('Deployment', () => {
@@ -21,11 +23,11 @@ describe('RequestsManager Contract', () => {
         let requestsManager : RequestsManager;
         let requests : MockContract;
         let authorization : MockContract;
-        let deployer : SignerWithAddress;
+        let deployer : Wallet;
         let activity : MockContract;
         let players : MockContract;
-        let secondAdmin : SignerWithAddress;
-        let nonAdmin : SignerWithAddress;
+        let secondAdmin : Wallet;
+        let nonAdmin : Wallet;
 
         const adminBytes = ethers.utils.keccak256(ethers.utils.toUtf8Bytes('ADMIN'));
         const activityPoints = 1;
@@ -33,18 +35,17 @@ describe('RequestsManager Contract', () => {
 
         describe('Review request', () => {
             beforeEach(async () => {
-                deployer = await ethers.getNamedSigner('deployer');
-                [secondAdmin, nonAdmin] = await ethers.getUnnamedSigners();
+                [deployer, secondAdmin, nonAdmin] = waffle.provider.getWallets();
 
-                authorization = await deployMockContract(deployer, Authorization__factory.abi);
+                authorization = await waffle.deployMockContract(deployer, Authorization__factory.abi);
                 await authorization.mock.ADMIN.returns(adminBytes);
                 await authorization.mock.ensureHasRole.withArgs(adminBytes, deployer.address).returns();
                 await authorization.mock.ensureHasRole.withArgs(adminBytes, secondAdmin.address).returns();
                 await authorization.mock.ensureHasRole.withArgs(adminBytes, nonAdmin.address).reverts();
 
-                activity = await deployMockContract(deployer, Activity__factory.abi);
+                activity = await waffle.deployMockContract(deployer, Activity__factory.abi);
 
-                requests = await deployMockContract(deployer, Requests__factory.abi);
+                requests = await waffle.deployMockContract(deployer, Requests__factory.abi);
                 await requests.mock.review.withArgs(requestId, 1).returns();
                 await requests.mock.review.withArgs(requestId, 2).returns();
                 await requests.mock.getRequest.withArgs(requestId).returns({
@@ -54,11 +55,11 @@ describe('RequestsManager Contract', () => {
                     status: 0,
                 });
 
-                players = await deployMockContract(deployer, Players__factory.abi);
+                players = await waffle.deployMockContract(deployer, Players__factory.abi);
                 await players.mock.addPoints.reverts();
                 await players.mock.substractPoints.reverts();
 
-                requestsManager = await new RequestsManager__factory(deployer).deploy(authorization.address, requests.address, players.address);
+                requestsManager = (await waffle.deployContract(deployer, RequestsManager__factory, [authorization.address, requests.address, players.address])) as RequestsManager;
             });
 
             it('Should revert when non-admin tries to review', async () => {
@@ -76,14 +77,13 @@ describe('RequestsManager Contract', () => {
             it('should call addPoints when changing status to approved', async () => {
                 await players.mock.addPoints.withArgs(deployer.address, activityPoints).returns();
 
-                expect(await requestsManager.connect(secondAdmin).review(requestId, 1)).to.be.undefined;
+                await expect(requestsManager.connect(secondAdmin).review(requestId, 1)).to.emit(requestsManager, 'RequestReviewed').withArgs(secondAdmin.address, requestId, 1);
             });
 
             it('should call substractPoints when changing status to rejected', async () => {
                 await players.mock.substractPoints.withArgs(deployer.address, activityPoints).returns();
 
-                await requestsManager.connect(secondAdmin).review(requestId, 2);
-                expect('substractPoints').to.be.calledOnContractWith(requests, [requestId, 2]);
+                await expect(requestsManager.connect(secondAdmin).review(requestId, 2)).to.emit(requestsManager, 'RequestReviewed').withArgs(secondAdmin.address, requestId, 2);
             });
         })
     });
