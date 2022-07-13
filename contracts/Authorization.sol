d3318ac96b7d4b596093b0ea9251bd83ca3bea00
Maciej Czekaj <mczekaj@valuelogic.one>
2022-07-13 12:40:51 +0200

    #14 cleanup

--- contracts/Authorization.sol
+++ contracts/Authorization.sol
@@ -4,19 +4,10 @@ pragma solidity ^0.8.8;
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "./libraries/Roles.sol";
 
-error Authorization__MissingRole(bytes32 role, address account);
-error Authorization__MissingOneOfRoles(bytes32[] roles, address account);
-
 contract Authorization is AccessControl {
     constructor() {
         _grantRole(Roles.ADMIN, msg.sender);
         _setRoleAdmin(Roles.ADMIN, Roles.ADMIN);
         _setRoleAdmin(Roles.PLAYER, Roles.ADMIN);
-    }
-
-    function ensureHasRole(bytes32 role, address account) external view {
-        if(!hasRole(role, account)) {
-            revert Authorization__MissingRole(role, account);
-        }
     }
 }
